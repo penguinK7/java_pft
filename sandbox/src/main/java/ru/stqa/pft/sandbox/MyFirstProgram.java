b248fffd3465302e4238155a4021999aedca5301
penguinK7 <klenkova7@gmail.com>
2021-03-02 23:50:18 +0300

    Программа для расчета расстояние между точками, функция в main

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,21 +3,15 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        hello("world");
-        hello("Ksenia");
 
-        Square s = new Square(8);
-
-        System.out.println("Площадь квадрата со строной " + s.l + "=" + s.area());
-Rectangle r = new Rectangle(4, 12);
-
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+        Point P = new Point(0, 15, 7, -2);
+        System.out.println("Расстояние между точками = " + distance(P));
     }
-
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody +"!");
+    public static double distance (Point P){
+        return Math.sqrt((P.p3-P.p1)*(P.p3-P.p1)+(P.p4-P.p2)*(P.p4-P.p2));
     }
 
 
 
 }
+
