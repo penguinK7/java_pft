a37de96bbb0c47fdfb8fdced4752399a6c16ab4b
penguinK7 <klenkova7@gmail.com>
2021-03-02 21:52:00 +0300

    Площадь квадрата и переменные

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,11 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String somebody = "world!";
+        System.out.println("Hello, " + somebody);
+        double l = 145.5;
+        double s = l*l;
+        System.out.println("Площадь квадрата со строной " + l + " = " + s);
 
     }
 }
