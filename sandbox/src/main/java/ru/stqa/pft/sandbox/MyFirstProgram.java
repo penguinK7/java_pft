56b2b7a6426daf3bc1c5f8349b8414ab75d61d39
penguinK7 <klenkova7@gmail.com>
2021-03-03 12:51:15 +0300

    добавила this.x

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,13 +1,11 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+    public static void main (String[] args){
 
-    public static void main(String[] args) {
-
-        Point p1 = new Point(2.0, 11);
-        Point p2 = new Point(3, -3);
-        System.out.println("Расстояние между точками p1 (" + p1.x + ";" + p1.y + ") и p2 (" + p2.x + ";" + p2.y + ") = " + p1.distance(p1, p2));
-        System.out.println("Расстояние между точками p1 (" + p1.x + ";" + p1.y + ") и p2 (" + p2.x + ";" + p2.y + ") = " + p2.distance(p1, p2));
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(2, 1);
+        System.out.println("Расстояние между точками p1 (" + p1.x + ";" + p1.y + ") и p2 (" + p2.x + ";" + p2.y + ")= " + p2.distance(p1));
     }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.sandbox;
 
-
 public class Point {
     public double x;
     public double y;
@@ -9,9 +8,9 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-    public double distance(Point p1, Point p2){
-        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
 
+    public double distance(Point p1){
+        return Math.sqrt((this.x - p1.x) * (this.x - p1.x) + (this.y - p1.y) * (this.y - p1.y));
     }
 
 }
