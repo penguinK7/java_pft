ca00dcba182fd86018c5e9de817b5ab1b20892e1
penguinK7 <klenkova7@gmail.com>
2021-03-03 09:28:04 +0300

    расстояние между точками, ф-ция в майн, два атрибута

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,12 +4,19 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        Point P = new Point(0, 8, 6, -2);
-        System.out.println("Расстояние между точками = " + P.distance());
+        Point p1 = new Point(2.0, 4);
+        Point p2 = new Point(3, 5);
+        System.out.println("Расстояние между тчками p1 (" + p1.x + ";" + p1.y + ") и p2 (" + p2.x + ";" + p2.y + ") = " + distance(p1, p2));
+
     }
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
 
-
-
-
+    }
 }
+
+
+
+
+
 
