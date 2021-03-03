101eb6bcc0f5e4f5051169dd9f4fbce4c785dcde
penguinK7 <klenkova7@gmail.com>
2021-03-03 09:37:44 +0300

    расстояние между точками, два атрибута, ф-ция в классе

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -9,6 +9,10 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+    public double distance(Point p1, Point p2){
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+
+    }
 
 }
 
