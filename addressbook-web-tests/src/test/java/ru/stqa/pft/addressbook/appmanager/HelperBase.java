0abeebb4de1d8dfd7e4ecfa79359b16041593a1d
penguinK7 <klenkova7@gmail.com>
2021-03-18 21:47:40 +0300

    создали тест для изменения групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -39,4 +39,7 @@ public class HelperBase {
             return false;
         }
     }
+
+
+
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.groupData;
+
+public class GroupModificationTests extends TestBase{
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new groupData("test", "test1", "test2"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
