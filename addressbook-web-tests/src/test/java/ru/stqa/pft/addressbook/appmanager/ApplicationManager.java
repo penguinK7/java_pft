7492f329537d5908fb5d05828cb947640dd27cb5
penguinK7 <klenkova7@gmail.com>
2021-03-21 20:58:58 +0300

    сделали возможность открывать тесты из разных браузеров 3.3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,20 +3,43 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
+        if (browser == BrowserType.FIREFOX ){
         wd = new FirefoxDriver();
+        }
+        else if(browser == BrowserType.CHROME){
+            wd = new ChromeDriver();
+        }else if( browser == BrowserType.IE){
+            wd = new InternetExplorerDriver();
+        }
+       /* else if( browser == BrowserType.EDGE){
+            wd = new EdgeDriver();
+        }*/
+
+
+
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook");
         groupHelper = new GroupHelper(wd);
