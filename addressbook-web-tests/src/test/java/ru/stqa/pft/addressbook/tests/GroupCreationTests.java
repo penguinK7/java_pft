ff45a9f7e3fa39e9b09b50f4787e7445cd6eff81
penguinK7 <klenkova7@gmail.com>
2021-04-03 22:21:47 +0300

    Лекция 4.9
    Вычисление максимального ид

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.groupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,13 +22,15 @@ public class GroupCreationTests extends TestBase {
         Assert.assertEquals(after.size(), before.size() + 1);
 
 
-        int max = 0;
+     /*  int max = 0;
         for (groupData g : after){
             if (g.getId() > max){
                 max = g.getId();
             }
-        }
-        group.setId(max);
+        }*/
+      //  Comparator<? super groupData> byId = (Comparator<groupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+      //  int max = after.stream().max( (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId(); //сравниваем по ид
+        group.setId(after.stream().max( (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     }
