a82c113959d49530ca71d89aac164c530baeb1b9
penguinK7 <klenkova7@gmail.com>
2021-05-10 13:27:47 +0300

    8.6 не работает

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HelperBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HelperBase.java
@@ -1,11 +1,15 @@
 package ru.stqa.pft.mantis.appmanager;
 
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 import java.io.File;
 
 public class HelperBase {
+
     protected ApplicationManager app;
     protected WebDriver wd;
 
@@ -18,20 +22,39 @@ public class HelperBase {
         wd.findElement(locator).click();
     }
 
-    protected void type (By locator, String text) {
+    protected void type(By locator, String text) {
         click(locator);
         if (text != null) {
             String existingText = wd.findElement(locator).getAttribute("value");
-            if (! text.equals(existingText)) {
+            if (!text.equals(existingText)) {
                 wd.findElement(locator).clear();
                 wd.findElement(locator).sendKeys(text);
             }
         }
     }
+
     protected void attach(By locator, File file) {
         if (file != null) {
             wd.findElement(locator).sendKeys(file.getAbsolutePath());
         }
     }
 
+    public boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    protected boolean isElementPresent(By locator) {
+        try {
+            wd.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+
+        }
+    }
 }
