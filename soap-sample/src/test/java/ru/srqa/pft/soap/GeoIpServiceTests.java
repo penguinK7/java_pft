4903c4dd71423bbe32933a955110fa3b8d02ce49
penguinK7 <klenkova7@gmail.com>
2021-05-23 22:40:43 +0300

    Лекция 9.2 непонятно

---
+++ soap-sample/src/test/java/ru/srqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,21 @@
+package ru.srqa.pft.soap;
+
+import com.lavasoft.GeoIPService;
+import org.testng.annotations.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testGeoIpService() {
+        String ipLocation20 = new GeoIPService().getGeoIPServiceSoap12().getCountryISO2ByName("USA");
+        assertEquals(ipLocation20, "<GeoIP><Country>US</Country></GeoIP>");
+    }
+
+    //String isoCountryName = new GeoIPService().getGeoIPServiceSoap12().getCountryISO2ByName("USA");
+    //assertEquals(isoCountryName, "<GeoIP><Country>US</Country></GeoIP>");
+
+}
